cb82ef5a5740fe9ded1740a093cfa962698dd3ed
Lucas Polo <pradopolo@gmail.com>
2018-04-10 20:13:45 -0300

    Criado atributos nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+
+    def __init__(self, nome=None, idade=18):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimetar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano')
     print(Pessoa.cumprimetar(p))
     print(id(p))
     print(p.cumprimetar())
+    print(p.nome)
+    print(p.idade)
